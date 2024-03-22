17a45c32a00c409d3e26ab390ee357df7baf3e6f
Tasdiqul Huq <107925819+TaurusSilver201@users.noreply.github.com>
2024-03-22 11:25:49 +0600

    Update Scarecrow.py

--- Scarecrow.py
+++ Scarecrow.py
@@ -1,48 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
+def max_distance(points):
+    points.sort(key=lambda x: x[0])  # Sort by x-coordinate
+    max_diff_x = max(points[i][0] - points[i - 1][0] for i in range(1, len(points)))
 
-typedef struct {
-    int x, y;
-} Point;
+    points.sort(key=lambda x: x[1])  # Sort by y-coordinate
+    max_diff_y = max(points[i][1] - points[i - 1][1] for i in range(1, len(points)))
 
-int cmp_x(const void *a, const void *b) {
-    Point *p1 = (Point *)a, *p2 = (Point *)b;
-    return p1->x - p2->x;
-}
+    return max_diff_x * max_diff_y
 
-int cmp_y(const void *a, const void *b) {
-    Point *p1 = (Point *)a, *p2 = (Point *)b;
-    return p1->y - p2->y;
-}
-
-int main() {
-    int t, n;
-    Point *points;
-    scanf("%d", &t);
-
-    while (t--) {
-        scanf("%d", &n);
-        points = (Point *)malloc(n * sizeof(Point));
-
-        for (int i = 0; i < n; i++) {
-            scanf("%d %d", &points[i].x, &points[i].y);
-        }
-
-        qsort(points, n, sizeof(Point), cmp_x);
-        int max_diff_x = 0;
-        for (int i = 1; i < n; i++) {
-            max_diff_x = max_diff_x > points[i].x - points[i - 1].x ? max_diff_x : points[i].x - points[i - 1].x;
-        }
-
-        qsort(points, n, sizeof(Point), cmp_y);
-        int max_diff_y = 0;
-        for (int i = 1; i < n; i++) {
-            max_diff_y = max_diff_y > points[i].y - points[i - 1].y ? max_diff_y : points[i].y - points[i - 1].y;
-        }
-
-        printf("%d\n", max_diff_x * max_diff_y);
-        free(points);
-    }
-
-    return 0;
-}
+cases = int(input())
+for _ in range(cases):
+    n = int(input())
+    points = [tuple(map(int, input().split())) for _ in range(n)]
+    print(max_distance(points))
